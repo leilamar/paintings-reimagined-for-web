583057f45336234825cfad21051535495a85134f
Leila Mardoum <lsmardoum@gmail.com>
2019-05-02 15:13:01 -0400

    rename prototype, begin adding color transitions to gallery page

--- navigation.js
+++ navigation.js
@@ -1,7 +1,9 @@
-console.log("in js")
 const frame = document.querySelector("#container");
 const images = document.querySelectorAll('img');
+const body = document.querySelector('body');
+console.log("body", body);
 
+/* TODO delete */
 function changeOpacity(event) {
   if (event.target.tagName == 'IMG') { // only work with img elements
     for (let i = 0; i < images.length; i++) {
@@ -20,7 +22,19 @@ function changeOpacityInd(event) {
     event.currentTarget.className = "opaque"; // keep selected image opaque
     event.currentTarget.addEventListener('mouseleave', resetOpacity);
     event.stopPropagation();
-  }
+}
+
+function changeOpacityIndColor(event, color) {
+    console.log('inside change opacity and color fn')
+    for (let i = 0; i < images.length; i++) {
+        images[i].className = 'semitransparent'; // change all frame image classes
+    }
+    event.currentTarget.className = "opaque"; // keep selected image opaque
+    body.style.backgroundColor = 'blue';
+    console.log(body.style.backgroundColor);
+    event.currentTarget.addEventListener('mouseleave', resetOpacityAndColor);
+    event.stopPropagation();
+}
   
 
 function resetOpacity() {
@@ -29,5 +43,11 @@ function resetOpacity() {
   }
 }
 
-// frame.addEventListener('mouseover', changeOpacity, false);
+function resetOpacityAndColor() {
+    for (let i = 0; i < images.length; i++) {
+      images[i].className = 'opaque'; // change all frame image classes
+    }
+    body.style.backgroundColor = 'white';
+  }
+
 images.forEach(image => image.addEventListener('mouseenter', changeOpacityInd));
